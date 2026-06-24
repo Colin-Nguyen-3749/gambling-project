d6f992e48d65ecb630787c4547a929ca72077a82
Colin Nguyen <nguyen_c9@denison.edu>
2026-06-24 11:48:59 -0400

    Define more global variables

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4, 
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    
+
+
 
 # collects user input that gets 
 # the deposit from the user
